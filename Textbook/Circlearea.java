eabff21b9fe4718d4f9066f383d7a2861f036870
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-02-20 20:46:42 -0500

    Add files via upload

---
+++ Textbook/Circlearea.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Circlearea{
+
+   
+	public static void main(String args[]) {
+		
+	Scanner rad = new Scanner(System.in);
+	System.out.println("\nType the radius: ");
+     
+    double radius = rad.nextDouble();
+      
+    double area = Math.PI * (radius * radius);
+    System.out.println("\nThe area of circle = " + area);
+      
+	double circum = Math.PI * 2*radius;
+    System.out.println( "\nThe circumference of circle = "+circum) ;
+
+  }
+  
+}
